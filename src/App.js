11a51eb055a3b3dfdad8b666a8c6d1f3ddb86c37
C Sai Anand <sai.anand2020@vitbhopal.ac.in>
2023-12-15 19:33:58 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import { BrowserRouter as Router, Route, Routes,Navigate } from "react-router-dom";
 
 function App() {
   return (
